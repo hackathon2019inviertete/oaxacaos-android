c28c2166f2d2a56a413e827eff12c00e4f5befe3
chronos <nike19942009@hotmail.com>
2019-05-24 16:46:57 -0500

    ui login terminada

--- app/src/main/java/com/example/oaxacaos/LoginOptionsActivity.java
+++ app/src/main/java/com/example/oaxacaos/LoginOptionsActivity.java
@@ -3,8 +3,13 @@ package com.example.oaxacaos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.example.oaxacaos.Models.UXMethods;
 
 public class LoginOptionsActivity extends AppCompatActivity {
 
@@ -13,9 +18,9 @@ public class LoginOptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_options);
 
-        Button loginBtn = findViewById(R.id.logInButton);
-        Button testBtn = findViewById(R.id.testButton);
-        Button regBtn = findViewById(R.id.registerButton);
+        Button loginBtn = findViewById(R.id.login_button);
+        EditText passwordEt = findViewById(R.id.login_password_et);
+        LinearLayout regLl = findViewById(R.id.register_ll);
 
         // click listeners para los botones
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -25,19 +30,21 @@ public class LoginOptionsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        testBtn.setOnClickListener(new View.OnClickListener() {
+        regLl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginOptionsActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginOptionsActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
-        regBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginOptionsActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        // login al hacer click en enter
+        UXMethods.performClickOnEnterPressed(passwordEt, loginBtn);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        ev = UXMethods.dispatchTouchEvent(this, ev);
+        return super.dispatchTouchEvent(ev);
     }
 }
--- app/src/main/java/com/example/oaxacaos/RegisterActivity.java
+++ app/src/main/java/com/example/oaxacaos/RegisterActivity.java
@@ -1,7 +1,14 @@
 package com.example.oaxacaos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.oaxacaos.Models.UXMethods;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button regBtn = findViewById(R.id.reg_btn);
+        EditText passwordEt = findViewById(R.id.reg_password_et);
+
+        // click listeners para los botones
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // registro al hacer click en enter
+        UXMethods.performClickOnEnterPressed(passwordEt, regBtn);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        ev = UXMethods.dispatchTouchEvent(this, ev);
+        return super.dispatchTouchEvent(ev);
     }
 }
